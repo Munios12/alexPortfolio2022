db58a7fa4e2cdcdcb4f00e2dcc3e23daab4a97d7
Alejandro Muñoz <alex12munozgeo@hotmail.es>
2022-05-31 16:38:04 +0200

    Add tech icons and photo, fix scroll

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -11,41 +11,75 @@ export const AboutMe = () => {
                         Encantado de que estes aquí. Mi nombre es Alejandro y
                         estoy estudiando programacion web en ISDI Coders
                         Academy. Mis inicios en la programacion web se remontan
-                        a 2020 debido a un sentimiento reorientacion de mi vida.
-                        A pesar de que soy Geologo, siempre he tenido la
-                        incertidumbre de si eso era lo que realmente queria.
+                        a 2020 debido a un sentimiento de reorientacion de mi
+                        vida. A pesar de mi carrera como
+                        <span className="sectionAbout__resaltedBio">
+                            {" "}
+                            geologo
+                        </span>
+                        , siempre he tenido la incertidumbre de si eso era lo
+                        que realmente quería.
                     </p>
                     <p className="sectionAbout__bio bio2">
-                        Me introduje gracias a dos amigos que se dedican a
-                        programar. Ello me llevo a estudiar primero Python pero
-                        rapipademente ya estaba estudiando HTML, CSS y JS.
-                        Comencé con cursor en Udemy que recuerdo con mucho
-                        cariño. Ahi fue donde realice mis primeros proyectos
+                        Me introduje gracias a dos amigos que se dedican a la
+                        programacion web. Ello me llevó a estudiar primero
+                        Python pero rápipademente ya estaba estudiando HTML, CSS
+                        y JS. Comencé con cursos en Udemy que recuerdo con mucho
+                        cariño, especialmente los de
+                        <span className="sectionAbout__resaltedBio">
+                            {" "}
+                            Jonas Schmedtmann
+                        </span>
+                        . Justo ahí fue donde realicé mis primeros proyectos
                         personales.
                     </p>
                     <p className="sectionAbout__bio bio3">
-                        Mas adelante, una vez ya tenia una buena base, comence
-                        con React. Varios de los proyectos que hice entonces,
-                        los econtraras en este portfolio. Supe que el siguiente
-                        paso, pasaba por estudiar un bootcamp. Actualmente estoy
-                        en
+                        Mas adelante, una vez ya tenia una buena base, comencé
+                        con React. Varios de los proyectos que hice por
+                        entonces, los econtrarás en este portfolio. Supe que el
+                        siguiente paso, pasaba por estudiar un bootcamp.
+                        Actualmente estoy en
                         <span className="sectionAbout__resaltedBio">
                             {" "}
                             busqueda activa de trabajo
                         </span>
                         .
                     </p>
+                    <p className="sectionAbout__bio">
+                        Estos son algunas de las tecnologías que más domino:
+                    </p>
                 </div>
 
-                <ul>
-                    <li>Javascript (ES6+)</li>
-                    <li>React</li>
-                    <li>Node.js</li>
-                    <li>Redux</li>
-                    <li>SCSS</li>
-                    <li>Firebase</li>
-                    <li>MongoDB</li>
+                <ul className="technologies-list">
+                    <li className="tech-1">
+                        <i className="devicon-javascript-plain colored"></i>
+                    </li>
+                    <li>
+                        <i className="devicon-react-original-wordmark colored"></i>
+                    </li>
+                    <li>
+                        <i className="devicon-nodejs-plain-wordmark colored"></i>
+                    </li>
+                    <li>
+                        <i className="devicon-redux-original colored"></i>
+                    </li>
+                    <li>
+                        <i className="devicon-sass-original colored"></i>
+                    </li>
+                    <li>
+                        <i className="devicon-firebase-plain-wordmark colored"></i>
+                    </li>
+                    <li>
+                        <i className="devicon-mongodb-plain-wordmark colored"></i>
+                    </li>
                 </ul>
+                <div className="photo-container">
+                    <img
+                        width={270}
+                        height={270}
+                        src={require("../images/alehandro.jpg")}
+                    />
+                </div>
             </section>
         </>
     );
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,14 +2,14 @@ import React, { Fragment, useEffect, useState } from "react";
 import "./header_styles.scss";
 
 export const Header = () => {
-    const hamburguer = document.querySelector(".header__nav");
-    const section = document.querySelector(".section");
-    const asideMenu = document.querySelector(".aside__menu");
-
-    const [hamAnim, setHamAnim] = useState(false);
+    const [hamAnim, setHamAnim] = useState(true);
     const [isHidden, setIsHidden] = useState(true);
 
     const showAnim = () => {
+        const hamburguer = document.querySelector(".header__nav");
+        const section = document.querySelector(".section");
+        const asideMenu = document.querySelector(".aside__menu");
+        const body = document.querySelector("body");
         setHamAnim(!hamAnim);
         setIsHidden(!isHidden);
 
@@ -23,12 +23,45 @@ export const Header = () => {
             asideMenu.classList.add("hidden");
         }
 
-        if (!isHidden) {
+        if (isHidden) {
             asideMenu.style.visibility = "visible";
             asideMenu.style.transform = "translateX(0vw)";
+            body.style.overflow = "hidden";
         } else {
             asideMenu.style.visibility = "hidden";
             asideMenu.style.transform = "translateX(100vw)";
+            body.style.overflow = "scroll";
+        }
+    };
+
+    const handleAnchorTags = () => {
+        const hamburguer = document.querySelector(".header__nav");
+        const asideMenu = document.querySelector(".aside__menu");
+        const body = document.querySelector("body");
+        const section = document.querySelector(".section");
+        const about = document.querySelector(".about a");
+
+        setHamAnim(!hamAnim);
+        setIsHidden(!isHidden);
+
+        if (hamAnim) {
+            hamburguer.classList.add("active");
+            section.classList.add("is-cristal");
+            asideMenu.classList.remove("hidden");
+        } else {
+            hamburguer.classList.remove("active");
+            section.classList.remove("is-cristal");
+            asideMenu.classList.add("hidden");
+        }
+
+        if (isHidden) {
+            asideMenu.style.visibility = "visible";
+            asideMenu.style.transform = "translateX(0vw)";
+            body.style.overflow = "hidden";
+        } else {
+            asideMenu.style.visibility = "hidden";
+            asideMenu.style.transform = "translateX(100vw)";
+            body.style.overflow = "scroll";
         }
     };
 
@@ -36,6 +69,7 @@ export const Header = () => {
         <Fragment>
             <header className="header">
                 <h1 className="header__title">Ñ</h1>
+
                 <button onClick={showAnim} className="header__nav">
                     <div className="nav__hamburguer-menu"></div>
                     <div className="nav__hamburguer-menu"></div>
@@ -44,16 +78,18 @@ export const Header = () => {
                 <aside className="aside__menu" ishidden="false">
                     <nav className="aside__nav">
                         <ol className="asideMenu__list">
-                            <li className="list__item">
+                            <li className="list__item home">
                                 <a href="#">Home</a>
                             </li>
-                            <li className="list__item">
-                                <a href="#">About</a>
+                            <li className="list__item about">
+                                <a href="#aboutMe" onClick={handleAnchorTags}>
+                                    About
+                                </a>
                             </li>
-                            <li className="list__item">
+                            <li className="list__item works">
                                 <a href="#">Works</a>
                             </li>
-                            <li className="list__item">
+                            <li className="list__item contact">
                                 <a href="#">Contact</a>
                             </li>
                         </ol>
@@ -75,7 +111,9 @@ export const Header = () => {
                     and modern projects.
                 </p>
                 <div className="falseButton">
-                    <p className="falseButton__text">Sobre mi !</p>
+                    <p className="falseButton__text" onClick={handleAnchorTags}>
+                        All about me!
+                    </p>
                 </div>
             </section>
         </Fragment>
